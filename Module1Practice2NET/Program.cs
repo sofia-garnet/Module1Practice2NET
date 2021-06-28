e6dcf3c0af2295a7aee4313ba35a07facfc75b2d
AngrySonya <Photographersofi@gmail.com>
2021-06-28 21:40:00 +0300

    add basics

---
+++ Module1Practice2NET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module1Practice2NET
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
